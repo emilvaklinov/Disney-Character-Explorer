69abdbff821b24c7aace6927c8a666cf286468f0
Emil Vaklinov <emil_vaklinov@yahoo.com>
2024-03-20 12:48:26 +0000

    Networking and the Model created

---
+++ Disney Character Explorer/Models/Character.swift
@@ -0,0 +1,8 @@
+//
+//  Character.swift
+//  Disney Character Explorer
+//
+//  Created by Emil Vaklinov on 20/03/2024.
+//
+
+import Foundation
---
+++ Disney Character Explorer/Networking/Endpoint.swift
@@ -0,0 +1,18 @@
+//
+//  Endpoint.swift
+//  Disney Character Explorer
+//
+//  Created by Emil Vaklinov on 20/03/2024.
+//
+
+import SwiftUI
+
+struct Endpoint: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Endpoint()
+}
