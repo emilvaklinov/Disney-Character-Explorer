5afd84347d65f9988ce4f58281ce8b327038d850
Emil Vaklinov <emil_vaklinov@yahoo.com>
2024-03-20 12:32:34 +0000

    Initial Commit

---
+++ Disney Character Explorer/Disney_Character_ExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Disney_Character_ExplorerApp.swift
+//  Disney Character Explorer
+//
+//  Created by Emil Vaklinov on 20/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Disney_Character_ExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
