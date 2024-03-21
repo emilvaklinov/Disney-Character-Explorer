5afe2d7068d4de71d70e9c2e112b8405c562875e
Emil Vaklinov <emil_vaklinov@yahoo.com>
2024-03-21 13:36:55 +0000

    Creatting Search bar component and MOCK Json file

---
+++ Disney Character Explorer/Views/UIComponents/SearchBar.swift
@@ -0,0 +1,18 @@
+//
+//  SearchBar.swift
+//  Disney Character Explorer
+//
+//  Created by Emil Vaklinov on 21/03/2024.
+//
+
+import SwiftUI
+
+struct SearchBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchBar()
+}
