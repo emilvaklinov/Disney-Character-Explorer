5e36a4ec65c2bd7e8056d68bfc5bff4291ab7ce4
Emil Vaklinov <emil_vaklinov@yahoo.com>
2024-03-21 13:19:52 +0000

    Detail view has been updated and new UI components created

---
+++ Disney Character Explorer/Views/UIComponents/CharacterDetailSection.swift
@@ -0,0 +1,8 @@
+//
+//  CharacterDetailSection.swift
+//  Disney Character Explorer
+//
+//  Created by Emil Vaklinov on 21/03/2024.
+//
+
+import Foundation
---
+++ Disney Character Explorer/Views/UIComponents/DetailItemView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailItemView.swift
+//  Disney Character Explorer
+//
+//  Created by Emil Vaklinov on 21/03/2024.
+//
+
+import SwiftUI
+
+struct DetailItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailItemView()
+}
