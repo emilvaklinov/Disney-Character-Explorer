4bb9cad423f5a9a3280f92770597a981022dd761
Emil Vaklinov <emil_vaklinov@yahoo.com>
2024-03-22 16:50:57 +0000

    Truncuated the text has been added

---
+++ Disney Character Explorer/Extentions/Truncated+String.swift
@@ -0,0 +1,18 @@
+//
+//  Truncation+String.swift
+//  Disney Character Explorer
+//
+//  Created by Emil Vaklinov on 22/03/2024.
+//
+
+import SwiftUI
+
+struct Truncation_String: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Truncation_String()
+}
